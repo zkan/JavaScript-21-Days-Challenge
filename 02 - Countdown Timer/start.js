1b11e9a298380efe9d27765270f3e02e29705e8d
zkan <kan@prontomarketing.com>
2020-03-29 10:49:02 +0700

    :sparkles: Implement count down function :bear:

--- 02 - Countdown Timer/start.js
+++ 02 - Countdown Timer/start.js
@@ -1,4 +1,29 @@
 (() => {
-  // เริ่มเขียนโค้ด
+  const SECOND = 1000
+  const MINUTE = SECOND * 60
+  const HOUR = MINUTE * 60
+  const DAY = HOUR * 24
 
-})();
+  setElementInnerText = (id, text) => {
+    const element = document.getElementById(id)
+    element.innerText = text
+  }
+
+  countDown = () => {
+    const now = new Date().getTime()
+    const newYear = new Date('December 31, 2020 23:59:59').getTime()
+    const unixTimeLeft = newYear - now
+
+    setElementInnerText('days', Math.floor(unixTimeLeft / DAY))
+    setElementInnerText('hours', Math.floor(unixTimeLeft % DAY / HOUR))
+    setElementInnerText('minutes', Math.floor(unixTimeLeft % HOUR / MINUTE))
+    setElementInnerText('seconds', Math.floor(unixTimeLeft % MINUTE / SECOND))
+  }
+
+  run = () => {
+    countDown()
+    setInterval(countDown, SECOND)
+  }
+
+  run()
+})()
